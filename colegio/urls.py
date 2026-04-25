377e2837d3f2faa635aa8045c342758718167f35
Andre Bertolo <barata321@gmail.com>
2026-04-25 14:23:31 -0300

    Corrige rotas faltantes

--- colegio/urls.py
+++ colegio/urls.py
@@ -53,6 +53,7 @@ urlpatterns = [
 
      # ===== BUSCA ATIVA =====
     path('busca-ativa/', views.busca_ativa, name='busca_ativa'),
+    path('menu-teste/', views.busca_ativa, name='menu_teste'),
     path('marcar-busca-ativa/<int:pk>/', views.marcar_busca_ativa, name='marcar_busca_ativa'),
     path('marcar-todos-busca-ativa/', views.marcar_todos_busca_ativa, name='marcar_todos_busca_ativa'),
 
