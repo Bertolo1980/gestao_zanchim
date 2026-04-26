528d014bd7cdd3052744e03593919b869d17dbd5
Andre Bertolo <barata321@gmail.com>
2026-04-25 22:02:42 -0300

    Prepara configuracoes para producao

--- colegio/static/admin/js/ckeditor_init.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    if (document.getElementById('editor')) {
-        CKEDITOR.replace('editor', {
-            toolbar: 'Full',
-            height: 400,
-            width: '100%'
-        });
-    }
-});
